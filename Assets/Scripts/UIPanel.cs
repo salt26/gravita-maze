14124efb5854af7569726c8d23bd2fe94bb149a8
안단태 <48adt@naver.com>
2021-11-23 23:28:49 +0900

    UI panel background, fire sprite update

---
+++ Assets/Scripts/UIPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPanel : MonoBehaviour
+{
+    public Sprite panel1920x1080;
+    public Sprite panel2160x1080;
+    public Sprite panel2960x1440;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Screen.width / 9 * 16 == Screen.height)
+        {
+            GetComponent<Image>().sprite = panel1920x1080;
+        }
+        else if (Screen.width * 2 == Screen.height)
+        {
+            GetComponent<Image>().sprite = panel2160x1080;
+        }
+        else if (Screen.width == 1440 && Screen.height == 2960)
+        {
+            GetComponent<Image>().sprite = panel2960x1440;
+        }
+        else
+        {
+            GetComponent<Image>().sprite = panel1920x1080;
+        }
+    }
+}
