ca656e12211e93513ce3adce561d458df4d606ed
silee1103 <[silee1103@snu.ac.kr]>
2022-07-24 05:56:19 +0900

    add Tutorial script

---
+++ Assets/Scripts/TutorialGuideUI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialGuideUI : MonoBehaviour
+{
+    public enum Pivot { TopRight = 0, BottomRight = 1, TopLeft = 2 }
+
+    RectTransform myTransform;
+    Text text;
+
+
+    public void Initialize(Vector2 pivotPosition, float width, float height, Pivot pivot, string message)
+    {
+        text = GetComponentInChildren<Text>();
+        myTransform = GetComponent<RectTransform>();
+        switch (pivot)
+        {
+            case Pivot.TopRight:
+                myTransform.localPosition = new Vector2(pivotPosition.x - width / 2f, pivotPosition.y + height / 2f);
+                break;
+            case Pivot.BottomRight:
+                myTransform.localPosition = new Vector2(pivotPosition.x - width / 2f, pivotPosition.y - height / 2f);
+                break;
+            case Pivot.TopLeft:
+                myTransform.localPosition = new Vector2(pivotPosition.x + width / 2f, pivotPosition.y + height / 2f);
+                break;
+        }
+        myTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+        myTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+        text.text = message.Replace('\\', '\n');
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (true) // To do (TutorialGuide 에 만든 함수들 가져오기...)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+
+        }
+    }
+
+}
+
