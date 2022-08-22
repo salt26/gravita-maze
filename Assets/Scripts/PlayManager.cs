eb9992fec70bf35a2d5f6036cff551717f46317f
unknown <yangd1009@naver.com>
2022-08-22 19:40:33 +0900

    meta bug revised

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -1502,7 +1502,7 @@ public class PlayManager : MonoBehaviour
             try
             {
                 mapinfo = sr.ReadToEnd().Trim();
-                mapHash = GetHash(sha256Hash, mapinfo.Trim());
+                mapHash = GetHash(sha256Hash, mapinfo);
             }
             catch (Exception e)
             {
@@ -1587,12 +1587,16 @@ public class PlayManager : MonoBehaviour
                 else
                 {
                     //다를 경우
+                    fs.Close();
+                    fs = new FileStream(metaPath, FileMode.Create, FileAccess.Write, FileShare.None);
                     StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
                     fs.Position = 0;
                     sw.WriteLine("0");
                     sw.WriteLine("False");
                     sw.WriteLine(mapHash);
                     sw.Close();
+                    GameManager.mm.tryCount = 0;
+                    GameManager.mm.hasClearedOnce = false;
                 }
                 fs.Close();
             }
@@ -1600,7 +1604,7 @@ public class PlayManager : MonoBehaviour
             {
                 sr?.Close();
                 fs?.Close();
-                fileStream = new FileStream(metaPath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+                fileStream = new FileStream(metaPath, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
                 streamWriter = new StreamWriter(fileStream, Encoding.UTF8);
                 streamWriter.WriteLine("0");
                 streamWriter.WriteLine("False");
