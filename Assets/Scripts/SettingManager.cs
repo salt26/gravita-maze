b153c83276f20a6cc7ca7262078c7f3cf05e2dbf
silee1103 <silee1103@snu.ac.kr>
2023-05-29 01:55:40 +0900

    settingUI animation 완성....?

--- Assets/Scripts/SettingManager.cs
+++ Assets/Scripts/SettingManager.cs
@@ -6,22 +6,85 @@ using UnityEngine.SceneManagement;
 
 public class SettingManager : MonoBehaviour
 {
+    public GameObject CreditPanel;
+    public Button QuitButton;
+    public Button CreditButton;
+    public Button BackSettingButton;
+
+    public bool isCredit=false;
+    public bool isOnce = false;
+    Animator PanelAnimation;
+    private void Start()
+    {
+        PanelAnimation = CreditPanel.GetComponent<Animator>();
+        CreditPanel.SetActive(false);
+    }
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Escape)||Input.GetKeyUp(KeyCode.Return))
+        if (isCredit && !isOnce)
         {
-            GameManager.gm.PlayButtonSFX();
-            GameManager.gm.LoadMain();            
+            if (!isOnce)
+            {
+                isOnce = true;
+                QuitButton.interactable = false;
+                CreditButton.interactable = false;
+                CreditPanel.SetActive(true);
+                StartCoroutine(SoundEffect());
+            }
+            if (Input.GetKeyUp(KeyCode.Return))
+            {
+                isOnce = false;
+                isCredit = false;
+                StartCoroutine(OffCreditAnimation());
+                GameManager.gm.PlayButtonSFX();
+            }
+        }
+        else
+        {
+            if (QuitButton.interactable && (Input.GetKeyUp(KeyCode.Escape) || Input.GetKeyUp(KeyCode.Return)))
+            {
+                GameManager.gm.PlayButtonSFX();
+                GameManager.gm.LoadMain();
+            }
         }
     }
+    IEnumerator SoundEffect()
+    {
+        yield return new WaitForSeconds(56.0f/60.0f);
+        GameManager.gm.PlayFallSFX(1.0f);
+        yield return new WaitForSeconds(23.0f/60.0f);
+        GameManager.gm.PlayFallSFX(0.75f);
+        yield return new WaitForSeconds(14.0f/60.0f);
+        GameManager.gm.PlayFallSFX(0.5f);
+        yield return new WaitForSeconds(4.0f/60.0f);
+    }
+
+    IEnumerator OffCreditAnimation()
+    {
+        PanelAnimation.SetTrigger("BackSettingDown");
+        yield return new WaitForSeconds(1.5f);
+
+        QuitButton.interactable = true;
+        CreditButton.interactable = true;
+        CreditPanel.SetActive(false);
+    }
+
     public Dropdown languageSetting;
     public void BackMainButtonDown()
     {
         GameManager.gm.LoadMain();
     }
+    public void InSettingBackButtonDown()
+    {
+        isOnce = false;
+        StartCoroutine(OffCreditAnimation());
+        isCredit = false;
+    }
     public void CreditButtonDown()
     {
-        GameManager.gm.LoadCredit() ;
+        isCredit = true;
+        CreditPanel.SetActive(true);
+        StartCoroutine(SoundEffect());
     }
     public void ChangeLanguage()
     {
@@ -32,5 +95,10 @@ public class SettingManager : MonoBehaviour
     {
         GameManager.gm.PlayButtonSFX();
     }
+    public void PlayFallSFX(float volume)
+    {
+        GameManager.gm.PlayFallSFX(volume);
+    }
+
 
 }
